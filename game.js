3875da38cd2464bc97c6c30b95d582a4f3eb71fe
Joseph Adamo <josephaadamo67@gmail.com>
2022-03-23 13:37:30 -0500

    Moved everything except player initilization out of global scope.

--- game.js
+++ game.js
@@ -1,20 +1,3 @@
-const board = document.getElementById("board");
-const gamePieces = Array.from(document.querySelectorAll('.gamepiece'));
-const winnerDisplay = document.getElementById("winner");
-const player1Score = document.getElementById("player1Score");
-const player2Score = document.getElementById("player2Score");
-
-const player1Name = document.getElementById("player1");
-const player2Name = document.getElementById("player2");
-
-const formExpand = document.getElementById("expand-form");
-const formHolder = document.getElementById("form-holder");
-const formSubmit = document.getElementById("name-changer");
-
-const resetScore = document.getElementById("score-resetter");
-
-const resetButton = document.getElementById("reset");
-
 const playerFactory = (name, xo, turn, score) => {
     let playerName = name;
     let playerXO = xo;
@@ -45,12 +28,16 @@ const playerFactory = (name, xo, turn, score) => {
     return { getName, score, setTurn, takeTurn, getXO, setName, getTurn };
 };
 
+
 const player1 = playerFactory("Player 1", true, true, 0);
 const player2 = playerFactory("Player 2", false, false, 0);
+
 
 const gameBoard = (() => {
     'use strict';
     let tttBoard = ["", "", "", "", "", "", "", "", ""];
+    const gamePieces = Array.from(document.querySelectorAll('.gamepiece'));
+
 
     const resetBoard = () => {
         gameBoard.tttBoard = ["", "", "", "", "", "", "", "", ""];
@@ -61,6 +48,7 @@ const gameBoard = (() => {
 
 
 
+    // 5 functions to check if game is over. consolidated in _checkGameOVer
     const _checkRows = () => {
         for (let i = 0; i < 3; i++) {
             let tempRow = [];
@@ -116,47 +104,53 @@ const gameBoard = (() => {
             return false;
     }
 
-    const turn = (index) => {
-
-        gamePieces.forEach((piece, index) => piece.addEventListener('click', () => {
-            if (_checkGameOver() || gameBoard.tttBoard[index]) {
-                return 0;
-            }
-            if (player1.getTurn()) {
-                gameBoard.tttBoard[index] = player1.getXO();
-            } else {
-                gameBoard.tttBoard[index] = player2.getXO();
-            }
-            displayController.displayBoard();
-            if (_checkGameOver()) {
-                displayController.declareWinner();
-            } else {
-                player1.takeTurn();
-                player2.takeTurn();
-            }
-
-        }));
 
 
+    gamePieces.forEach((piece, index) => piece.addEventListener('click', () => {
+        if (_checkGameOver() || gameBoard.tttBoard[index]) {
+            return 0;
+        }
+        if (player1.getTurn()) {
+            gameBoard.tttBoard[index] = player1.getXO();
+        } else {
+            gameBoard.tttBoard[index] = player2.getXO();
+        }
+        displayController.displayBoard();
+        if (_checkGameOver()) {
+            displayController.declareWinner();
+        } else {
+            player1.takeTurn();
+            player2.takeTurn();
+        }
+
+    }));
 
 
-    };
+
     return {
         tttBoard,
         resetBoard,
-        turn,
+        gamePieces,
+
     };
 })();
 
 const displayController = (() => {
     'use strict';
+
+    const player1Name = document.getElementById("player1");
+    const player2Name = document.getElementById("player2");
+
+    const winnerDisplay = document.getElementById("winner");
+    const player1Score = document.getElementById("player1Score");
+    const player2Score = document.getElementById("player2Score");
+
     const displayBoard = () => {
-        gamePieces.forEach((piece, index) => {
+        gameBoard.gamePieces.forEach((piece, index) => {
             piece.innerHTML = gameBoard.tttBoard[index];
         })
-
-
     };
+
     const declareWinner = () => {
         if (player1.getTurn()) {
             winnerDisplay.innerHTML = player1.getName() + " won! Congratulations!";
@@ -170,42 +164,45 @@ const displayController = (() => {
         }
     }
 
+    const resetButton = document.getElementById("reset");
+
+    resetButton.addEventListener('click', () => {
+        gameBoard.resetBoard();
+        winnerDisplay.innerHTML = "Tic-Tac-Toe";
+    });
+    const formExpand = document.getElementById("expand-form");
+    const formHolder = document.getElementById("form-holder");
+    const formSubmit = document.getElementById("name-changer");
+
+    const resetScore = document.getElementById("score-resetter");
+    formExpand.addEventListener('click', () => {
+        if (formHolder.style.display === "none") {
+            formHolder.style.display = "block";
+        } else {
+            formHolder.style.display = "none";
+        }
+    });
+
+    formSubmit.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        let playerName1 = document.getElementById('player1Name').value;
+        let playerName2 = document.getElementById('player2Name').value;
+        player1.setName(playerName1);
+        player2.setName(playerName2);
+        player1Name.innerHTML = player1.getName();
+        player2Name.innerHTML = player2.getName();
+    });
+
+    resetScore.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        player1.score = 0;
+        player2.score = 0;
+        player1Score.innerHTML = player1.score;
+        player2Score.innerHTML = player2.score;
+
+    });
+
     return { displayBoard, declareWinner };
 })();
-
-
-resetButton.addEventListener('click', () => {
-    gameBoard.resetBoard();
-    winnerDisplay.innerHTML = "Tic-Tac-Toe";
-});
-
-formExpand.addEventListener('click', () => {
-    if (formHolder.style.display === "none") {
-        formHolder.style.display = "block";
-    } else {
-        formHolder.style.display = "none";
-    }
-});
-
-formSubmit.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    let playerName1 = document.getElementById('player1Name').value;
-    let playerName2 = document.getElementById('player2Name').value;
-    player1.setName(playerName1);
-    player2.setName(playerName2);
-    player1Name.innerHTML = player1.getName();
-    player2Name.innerHTML = player2.getName();
-});
-
-resetScore.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    player1.score = 0;
-    player2.score = 0;
-    player1Score.innerHTML = player1.score;
-    player2Score.innerHTML = player2.score;
-
-});
-
-gameBoard.turn();
